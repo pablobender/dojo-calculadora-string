4466c992aeccda4ec296e05a85e419948abee621
Pablo Bender <pablo@bender.eti.br>
2015-03-23 22:59:53 -0300

    removendo violação do rubocop

--- src/calculadora_string.rb
+++ src/calculadora_string.rb
@@ -7,15 +7,23 @@ class CalculadoraString
 
   def somar(entrada)
     soma = 0
-    requer_operando = false
+    @parametro_requerido = false
     entrada.each_char do |caractere_atual|
       caractere = Caractere.new caractere_atual
-      next if caractere.eh_zero
-      requer_operando = caractere.eh_delimitador @delimitador
-      throw :entrada_invalida unless caractere.caractere_valido || requer_operando
+      validar_caractere caractere
       soma += caractere.to_i
     end
-    throw :entrada_invalida if requer_operando
+    throw :entrada_invalida if @parametro_requerido
     soma
+  end
+
+  private
+
+  def validar_caractere(caractere)
+    unless caractere.eh_zero
+      @parametro_requerido = caractere.eh_delimitador @delimitador
+      throw :entrada_invalida unless caractere.caractere_valido || @parametro_requerido
+    end
+    true
   end
 end
