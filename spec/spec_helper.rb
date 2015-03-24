9c112e030a1b3ff9989a248b64663f3f7e7395c7
Pablo Bender <pablo@bender.eti.br>
2015-03-24 01:13:38 -0300

    retornando o relatório do simplecov + coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 # encoding utf-8
+require 'simplecov'
 require 'coveralls'
 
-Coveralls.wear!
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
 
 require File.expand_path('../../src/calculadora_string', __FILE__)
