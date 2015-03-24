e015e350121d19174707fe11253e4ff545951915
Pablo Bender <pablo@bender.eti.br>
2015-03-24 01:04:25 -0300

    re-config do coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,6 @@
 # encoding utf-8
 require 'coveralls'
-require 'simplecov'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-SimpleCov.start
 Coveralls.wear!
 
 require File.expand_path('../../src/calculadora_string', __FILE__)
